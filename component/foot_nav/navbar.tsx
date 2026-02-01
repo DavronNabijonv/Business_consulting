fc8bcbe3a791ef4909392480ae3f3cecad4bf913
nabijonovdavronbek619@gmail.com <nabijonovdavronbek619@gmail.com>
2026-02-01 10:09:57 +0500

    navbar bg color updated by page

--- component/foot_nav/navbar.tsx
+++ component/foot_nav/navbar.tsx
@@ -1,13 +1,19 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export function Navbar() {
-  const page = useSearchParams();
-  const bgColor = "";
+  const page = usePathname();
+  const bgColor =
+    page === "/service"
+      ? "bg-[#0D1F08]"
+      : page === "schedule"
+        ? "bg-[#1f0808]"
+        : "bg-[#080a1f]";
+  console.log("current page: ", page);
   return (
-    <div className="bg-[#0D1F08] font-poppins py-5">
+    <div className={`font-poppins py-5 ${bgColor}`}>
       <div className="max-w-350 w-full mx-auto flex items-center justify-between ">
         <Link href="/">
           <Image src="/Logo.png" alt="logo image" width={30} height={20} />
